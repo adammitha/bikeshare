57b365be73b431a8a58c8de2ba8590def4aed547
Adam Mitha <adam.mitha@gmail.com>
2022-05-06 10:45:57 -0700

    Use reqwest instead of hyper

--- src/status.rs
+++ src/status.rs
@@ -1,7 +1,7 @@
 use axum::{extract::Query, http::StatusCode, Json, response::IntoResponse};
-use hyper::Client;
-use hyper_tls::HttpsConnector;
 use serde::{Deserialize, Serialize};
+
+use crate::API_URL;
 
 #[derive(Deserialize)]
 pub struct StationQuery {
@@ -26,40 +26,36 @@ pub struct StationStatus {
 }
 
 pub async fn station_status(_query: Query<StationQuery>) -> Result<Json<StationStatus>, StatusError> {
-    let stations = fetch_stations(crate::API_URL.parse().unwrap()).await?;
+    let stations = fetch_stations(API_URL).await?;
     Ok(Json(stations.result[0].clone()))
 }
 
-async fn fetch_stations(uri: http::Uri) -> Result<StatusApiData, StatusError> {
-    let client = Client::builder().build::<_, hyper::Body>(HttpsConnector::new());
-    let response = client.get(uri)
-        .await
-        .map_err(|_| { StatusError::ApiFailure })?;
-    let body = hyper::body::to_bytes(response.into_body())
-        .await
-        .map_err(|_| { StatusError::ApiFailure })?;
-    Ok(serde_json::de::from_slice(&body)
-        .map_err(|_| { StatusError::Parse })?)
+async fn fetch_stations(uri: &str) -> Result<StatusApiData, StatusError> {
+    Ok(reqwest::get(uri)
+        .await?
+        .json::<StatusApiData>()
+        .await?)
 }
 
 /// Errors that can occur when retrieving the status of a bike station
 pub enum StatusError {
-    ApiFailure,
-    Parse,
+    ApiFailure(reqwest::Error),
 }
 
 impl IntoResponse for StatusError {
     fn into_response(self) -> axum::response::Response {
         let (status, message) = match self {
-            StatusError::ApiFailure => (
+            StatusError::ApiFailure(inner) => (
                 StatusCode::INTERNAL_SERVER_ERROR,
-                "Error fetching station status from the bike share API",
-            ),
-            StatusError::Parse => (
-                StatusCode::INTERNAL_SERVER_ERROR,
-                "Error parsing the response from the bike share API",
+                format!("Error fetching station status from the bike share API: {}", inner),
             ),
         };
         (status, message).into_response()
+    }
+}
+
+impl From<reqwest::Error> for StatusError {
+    fn from(err: reqwest::Error) -> Self {
+        StatusError::ApiFailure(err)
     }
 }
