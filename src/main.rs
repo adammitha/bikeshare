47f5e7be04c5652356a4c4cfb79e6e1b8d98a883
Adam Mitha <adam.mitha@gmail.com>
2022-05-07 08:12:05 -0700

    Log basic request/response information at INFO level

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,27 @@ use std::net::SocketAddr;
 
 use axum::{routing::get, Router};
 use bikeshare::status::station_status;
-use tower_http::trace::TraceLayer;
+use tower_http::trace::{DefaultOnRequest, DefaultOnResponse, TraceLayer};
+use tracing::Level;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::registry()
         .with(tracing_subscriber::EnvFilter::new(
-            std::env::var("RUST_LOG")
-                .unwrap_or_else(|_| "debug".into())
+            std::env::var("RUST_LOG").unwrap_or_else(|_| "debug".into()),
         ))
         .with(tracing_subscriber::fmt::layer())
         .init();
 
-    let app = Router::new()
-        .route("/status", get(station_status))
-        .layer(TraceLayer::new_for_http());
-    
+    let app = Router::new().route("/status", get(station_status)).layer(
+        TraceLayer::new_for_http()
+            .on_request(DefaultOnRequest::new().level(Level::INFO))
+            .on_response(DefaultOnResponse::new().level(Level::INFO)),
+    );
+
     let addr = "0.0.0.0:8080".parse::<SocketAddr>().unwrap();
-    tracing::debug!("Listening on {}", addr);
+    tracing::info!("Listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
