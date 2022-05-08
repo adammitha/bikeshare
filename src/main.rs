2a11ae5f3bdc14486c6cf914e96e777b2f95c24e
Adam Mitha <adam.mitha@gmail.com>
2022-05-08 08:20:17 -0700

    Add .envrc for api keys

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,11 @@ async fn main() {
 
     let app = Router::new().route("/status", get(station_status)).layer(
         TraceLayer::new_for_http()
-            .make_span_with(DefaultMakeSpan::new().level(Level::INFO).include_headers(true))
+            .make_span_with(
+                DefaultMakeSpan::new()
+                    .level(Level::INFO)
+                    .include_headers(true),
+            )
             .on_request(DefaultOnRequest::new().level(Level::INFO))
             .on_response(DefaultOnResponse::new().level(Level::INFO)),
     );
