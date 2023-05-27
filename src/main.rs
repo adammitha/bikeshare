0eadae2d8a16c731587d0bf6571d448be00c186f
Adam Mitha <adam.mitha@gmail.com>
2023-05-27 10:47:09 -0700

    Add metrics endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::{collections::HashMap, net::SocketAddr};
 
 use axum::{routing::get, Router};
+use axum_prometheus::PrometheusMetricLayer;
 use bikeshare::status::station_status;
 use opentelemetry_otlp::WithExportConfig;
 use opentelemetry_semantic_conventions::resource::SERVICE_NAME;
@@ -11,16 +12,21 @@ use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 #[tokio::main]
 async fn main() {
     setup_tracing();
-    let app = Router::new().route("/status", get(station_status)).layer(
-        TraceLayer::new_for_http()
-            .make_span_with(
-                DefaultMakeSpan::new()
-                    .level(Level::INFO)
-                    .include_headers(true),
-            )
-            .on_request(DefaultOnRequest::new().level(Level::INFO))
-            .on_response(DefaultOnResponse::new().level(Level::INFO)),
-    );
+    let (prometheus_layer, metrics_handler) = PrometheusMetricLayer::pair();
+    let app = Router::new()
+        .route("/status", get(station_status))
+        .route("/metrics", get(|| async move { metrics_handler.render() }))
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(
+                    DefaultMakeSpan::new()
+                        .level(Level::INFO)
+                        .include_headers(true),
+                )
+                .on_request(DefaultOnRequest::new().level(Level::INFO))
+                .on_response(DefaultOnResponse::new().level(Level::INFO)),
+        )
+        .layer(prometheus_layer);
 
     let addr = "0.0.0.0:8080".parse::<SocketAddr>().unwrap();
     tracing::info!("Listening on {}", addr);
